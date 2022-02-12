119619c0f990b8a418851a79e7a6aa96badcd329
AlexDoss42 <alex.doss42@gmail.com>
2022-02-11 23:59:39 -0700

    form basics

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,13 @@
+import Card from '../ui/Card';
+
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm() {
+    return <Card>
+        <form>
+
+        </form>
+    </Card>
+}
+
+export default NewMeetupForm;
