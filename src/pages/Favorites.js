2122552621ef09f60e4185055a25b53819e07b2b
AlexDoss42 <alex.doss42@gmail.com>
2022-02-14 20:48:55 -0700

    adds favorites list to the favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,15 @@
+import { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+import FavoritesContext from '../store/favorites-context';
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext);
+
+    return <section>
+        <h1>My Favorites</h1>
+        <MeetupList meetups={favoritesCtx.favorites}/>
+    </section>
 }
 
 export default FavoritesPage;
